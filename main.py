a4959352f44f8399e4117ad60bf3341696ac0c50
Haisenberg <1paf1@ukr.net>
2024-01-06 15:54:12 +0200

    second task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #hello
 
-try:
+'''try:
     user_select = int(input("Введіть число від 1 до 7:"))
     if user_select == 1:
         print("Понеділок")
@@ -20,3 +20,16 @@ try:
         print("Введене число не входить в діапазон від 1 до 7.")
 except ValueError:
     print("Введіть коректне число.")
+'''
+try:
+    num1 = float(input("Введіть перше число: "))
+    num2 = float(input("Введіть друге число: "))
+
+    if num1 == num2:
+        print("Числа рівні.")
+    else:
+        smaller, larger = min(num1, num2), max(num1, num2)
+        print(f"Числа не рівні. В порядку зростання:{smaller},{larger}")
+except ValueError:
+    print("Будь ласка, введіть коректні числа.")
+
